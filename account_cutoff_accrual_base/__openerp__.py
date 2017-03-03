8bf5b904cb2c681da9bde0a151a9662a45ec389c
zeroincombenze <info@shs-av.com>
2017-03-03 09:31:17 +0100

    [FIX] Update documentation

--- account_cutoff_accrual_base/__openerp__.py
+++ account_cutoff_accrual_base/__openerp__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'Account Accrual Base',
-    'version': '8.0.0.1.0',
+    'version': '7.0.0.1.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Base module for accrued expenses and revenues',
