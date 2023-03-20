39cfebf958fae51ccb03f4a945667777ec8a6653
zeroincombenze <info@shs-av.com>
2023-03-20 12:04:09 +0100

    [NEW] Initial setup 12.0

--- README.md
+++ README.md
@@ -1,52 +1,9 @@
-[![Build Status](https://travis-ci.org/zeroincombenze/account-closing.svg?branch=7.0)](https://travis-ci.org/zeroincombenze/account-closing)
-[![license agpl](https://img.shields.io/badge/licence-AGPL--3-blue.svg)](http://www.gnu.org/licenses/agpl-3.0.html)
-[![Coverage Status](https://coveralls.io/repos/github/zeroincombenze/account-closing/badge.svg?branch=7.0)](https://coveralls.io/github/zeroincombenze/account-closing?branch=7.0)
-[![codecov](https://codecov.io/gh/zeroincombenze/account-closing/branch/7.0/graph/badge.svg)](https://codecov.io/gh/zeroincombenze/account-closing/branch/7.0)
-[![OCA_project](http://www.zeroincombenze.it/wp-content/uploads/ci-ct/prd/button-oca-7.svg)](https://github.com/OCA/account-closing/tree/7.0)
-[![Tech Doc](http://www.zeroincombenze.it/wp-content/uploads/ci-ct/prd/button-docs-7.svg)](http://wiki.zeroincombenze.org/en/Odoo/7.0/dev)
-[![Help](http://www.zeroincombenze.it/wp-content/uploads/ci-ct/prd/button-help-7.svg)](http://wiki.zeroincombenze.org/en/Odoo/7.0/man/FI)
-[![try it](http://www.zeroincombenze.it/wp-content/uploads/ci-ct/prd/button-try-it-7.svg)](http://erp7.zeroincombenze.it)
-
-
-[![en](https://github.com/zeroincombenze/grymb/blob/master/flags/en_US.png)](https://www.facebook.com/groups/openerp.italia/)
-================================================================================================
-================================================================================================
+[![Build Status](https://travis-ci.org/OCA/account-closing.svg?branch=12.0)](https://travis-ci.org/OCA/account-closing)
+[![Coverage Status](https://img.shields.io/coveralls/OCA/account-closing.svg)](https://coveralls.io/r/OCA/account-closing?branch=12.0)
 
 Account Closing
 ===============
 
 This project aim to deal with modules related to manage account closing.
 
-[//]: # (copyright)
 
-----
-
-**Odoo** is a trademark of [Odoo S.A.](https://www.odoo.com/) (formerly OpenERP, formerly TinyERP)
-
-**OCA**, or the [Odoo Community Association](http://odoo-community.org/), is a nonprofit organization whose
-mission is to support the collaborative development of Odoo features and
-promote its widespread use.
-
-**zeroincombenze®** is a trademark of [SHS-AV s.r.l.](http://www.shs-av.com/)
-which distributes and promotes **Odoo** ready-to-use on its own cloud infrastructure.
-[Zeroincombenze® distribution](http://wiki.zeroincombenze.org/en/Odoo)
-is mainly designed for Italian law and markeplace.
-Everytime, every Odoo DB and customized code can be deployed on local server too.
-
-[//]: # (end copyright)
-[//]: # (addons)
-
-
-Available addons
-----------------
-addon | version | OCA version | summary
---- | --- | --- | ---
-[account_cutoff_accrual_base](account_cutoff_accrual_base/) | 0.1 | :repeat: | Base module for accrued expenses and revenues
-[account_cutoff_accrual_picking](account_cutoff_accrual_picking/) | 0.1 | :repeat: | Accrued Expense & Accrued Revenue from Pickings
-[account_cutoff_base](account_cutoff_base/) | 0.1 | :repeat: | Base module for Account Cut-offs
-[account_cutoff_prepaid](account_cutoff_prepaid/) | 0.1 | :repeat: | Prepaid Expense, Prepaid Revenue
-[account_multicurrency_revaluation](account_multicurrency_revaluation/) | 6.1 | :repeat: | Multicurrency revaluation
-
-[//]: # (end addons)
-
-[![chat with us](https://www.shs-av.com/wp-content/chat_with_us.gif)](https://tawk.to/85d4f6e06e68dd4e358797643fe5ee67540e408b)
--- account_multicurrency_revaluation/wizard/print_currency_unrealized_report.py
+++ account_multicurrency_revaluation/wizard/print_currency_unrealized_report.py
@@ -1,54 +1,43 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Guewen Baconnier, Yannick Vaucher
-#    Copyright 2012 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
-from openerp.osv import fields, orm
+# Copyright 2012-2018 Camptocamp SA
+# Copyright 2020 CorporateHub (https://corporatehub.eu)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-class UnrealizedCurrencyReportPrinter(orm.TransientModel):
+from odoo import models, api, fields, _
+from odoo.exceptions import ValidationError
+
+
+class UnrealizedCurrencyReportPrinter(models.TransientModel):
     _name = "unrealized.report.printer"
+    _description = 'Unrealized Currency Report Printer'
 
-    _columns = {
-        'chart_account_id': fields.many2one(
-            'account.account', 'Chart root',
-            domain=[('parent_id', '=', False)],
-            required=True),
-        'period_id': fields.many2one(
-            'account.period', 'Period to use', required=True),
-    }
+    account_ids = fields.Many2many(
+        'account.account',
+        string='Accounts',
+        domain=[('currency_revaluation', '=', True)],
+        default=lambda self: self._default_account_ids(),
+    )
 
-    def print_report(self, cursor, uid, wid, data, context=None):
+    def _default_account_ids(self):
+        account_model = self.env["account.account"]
+        account_ids = account_model.search([
+            ('currency_revaluation', '=', True)
+        ]).ids
+        return [(6, 0, account_ids)]
+
+    @api.multi
+    def print_report(self, data):
         """
         Show the report
         """
-        context = context or {}
-        # we update form with display account value
-        if isinstance(wid, list):
-            wid = wid[0]
-        current = self.browse(cursor, uid, wid, context=context)
-        form = {}
-        form['period_id'] = current.period_id.id
-        form['period_name'] = current.period_id.name
-        form['account_ids'] = [current.chart_account_id.id]
-        data['form'] = form
-
-        return {'type': 'ir.actions.report.xml',
-                'report_name': 'currency_unrealized',
-                'datas': data}
+        if self.account_ids:
+            docids = self.account_ids.ids
+            # in Odoo 11 we no longer call render, but report_action
+            # config should be false as otherwise it will call configuration
+            # wizard that works weirdly
+            return self.env.ref(
+                'account_multicurrency_revaluation.'
+                'action_report_currency_unrealized'
+            ).report_action(docids, config=False)
+        else:
+            raise ValidationError(_("Please, select the accounts!"))
